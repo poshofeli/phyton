e1bd524612bb08854e82ff1a563f7b6e73574959
eduardo <jesuedu050@gmail.com>
2026-06-03 18:53:32 -0400

    tarea hecha

--- tarea 3.py
+++ tarea 3.py
@@ -3,30 +3,43 @@ print ("Menu")
 user1 = "Edu"
 password2 = "jesus1020"
 
-for i in range (3):
-    if(i == 2):
+for i in range (4):
+    if(i == 3):
         print("Cantidad de intentos superada.")
         break
     user = input("ingrese usuario: ")
     password = input("ingrese contrasena: ")
     if(user == user1):
         if(password == password2 ):
-            print ("Inciaste.")
-            disponible =(int(input("Bienvenido, Cual es tu saldo? ")))
-            gastos = input("Cuantos gastos has hecho?")
+                print("Bienvenido al sistema.")
+                ingresos = input("Ingrese sus ingresos:")
+                while True:
+                    print("\n--- MENÚ DE OPCIONES ---")
+                    print("1. Mostrar saldo")
+                    print("2. Gestor de gastos")
+                    print("3. Salir")
+        
+                    opcion = input("Seleccione una opción (1-3): ")
 
-            acumulador = 0
+                    if opcion == "1":
+                        print("Su saldo es: " + str(ingresos))
+                    elif opcion == "2":
+                        gastos = input("Cuantos gastos has hecho?")
 
-            for i in range (int(gastos)):
-                gasto = input ("ingrese el gasto "+str(i+1)+":")
-                acumulador = acumulador + int(gasto)
+                        acumulador = 0
 
-            print ("hoy has gastado: " + str(acumulador),"y ten quedan: " + str(disponible-int(acumulador)))
-            break
+                        for i in range (int(gastos)):
+                            gasto = input ("ingrese el gasto "+str(i+1)+":")
+                            acumulador = acumulador + int(gasto)
+                        print("El total de gastos es: "+str(acumulador)," y su saldo restante es: "+str(int(ingresos)-acumulador))
+                    elif opcion == "3":
+                        print("Saliendo del programa...")
+                        break # Rompe el bucle while y termina
+                    else:
+                        print("Opción no válida. Intente de nuevo.")
+            
         else:
             print ("contraseña incorrecta.")
     else:
         print("usuario incorrecto.")
-
-
 
